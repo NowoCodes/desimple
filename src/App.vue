3c8700c07c88ec893ac71e8b00dd1c428451ae6e
Favour <toluwaniokunowo@gmail.com>
2021-11-30 09:23:10 +0100

    install starter

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue'
+
+let counter = ref(0)
+
+setInterval(() => {
+    counter.value++
+}, 1000)
+</script>
+
+<template>
+    <div>
+        <header class="text-gray-400 bg-gray-900 body-font">
+            <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+                    <svg class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" fill="none" stroke="currentColor" stroke-linecap="round"
+                         stroke-linejoin="round" stroke-width="2"
+                         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                    </svg>
+                    <span class="ml-3 text-xl">DeSimple</span>
+                </a>
+                <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                    <a class="mr-5 hover:text-white">First Link</a>
+                    <a class="mr-5 hover:text-white">Second Link</a>
+                    <a class="mr-5 hover:text-white">Third Link</a>
+                    <a class="mr-5 hover:text-white">Fourth Link</a>
+                </nav>
+            </div>
+        </header>
+
+        <!--        <header v-if="$route.meta.title" class="bg-white shadow">-->
+        <!--            <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">-->
+        <!--                <h1-->
+        <!--                    class="text-3xl font-bold leading-tight text-gray-900"-->
+        <!--                    @click="counter = 0"-->
+        <!--                >-->
+        <!--                    {{ $route.meta.title }} / {{ counter }}-->
+        <!--                </h1>-->
+        <!--            </div>-->
+        <!--        </header>-->
+        <main>
+            <router-view />
+        </main>
+    </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,63 @@
+<template>
+    <section class="text-gray-400 bg-gray-900 body-font">
+        <div class="mx-auto flex py-4 items-center justify-center flex-col">
+            <img alt="hero" class="lg:w-3/3 md:w-3/3 w-3/3 mb-10 object-cover object-center rounded"
+                 src="images/bg.jpg">
+            <div class="text-center lg:w-2/3 w-full">
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Cinematographer, Visual Designer, Creator</h1>
+                <p class="leading-relaxed mb-8">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing
+                    tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami
+                    snackwave tilde chillwave ugh. Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo
+                    booth af fingerstache pitchfork.</p>
+            </div>
+        </div>
+    </section>
+
+    <footer class="text-gray-400 bg-gray-900 body-font">
+        <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+            <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
+                <svg class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" fill="none" stroke="currentColor" stroke-linecap="round"
+                     stroke-linejoin="round" stroke-width="2"
+                     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                </svg>
+                <span class="ml-3 text-xl">DeSimple</span>
+            </a>
+            <p class="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">©
+                 2021
+                <a class="text-gray-500 ml-1" href="https://twitter.com/knyttneve" rel="noopener noreferrer"
+                   target="_blank">@bitsandnibble</a>
+            </p>
+            <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+      <a class="text-gray-400">
+        <svg class="w-5 h-5" fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+             viewBox="0 0 24 24">
+          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-400">
+        <svg class="w-5 h-5" fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+             viewBox="0 0 24 24">
+          <path
+              d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-400">
+        <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+             stroke-width="2" viewBox="0 0 24 24">
+          <rect height="20" rx="5" ry="5" width="20" x="2" y="2"></rect>
+          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+        </svg>
+      </a>
+      <a class="ml-3 text-gray-400">
+        <svg class="w-5 h-5" fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+             stroke-width="0" viewBox="0 0 24 24">
+          <path d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                stroke="none"></path>
+          <circle cx="4" cy="4" r="2" stroke="none"></circle>
+        </svg>
+      </a>
+    </span>
+        </div>
+    </footer>
+</template>
