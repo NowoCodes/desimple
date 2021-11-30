204a01bf529a699ae55e200768bbccf1e4b528ff
Favour <toluwaniokunowo@gmail.com>
2021-11-30 09:32:43 +0100

    change sitte name

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,29 +4,37 @@
             <img alt="hero" class="lg:w-3/3 md:w-3/3 w-3/3 mb-10 object-cover object-center rounded"
                  src="images/bg.jpg">
             <div class="text-center lg:w-2/3 w-full">
-                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Cinematographer, Visual Designer, Creator</h1>
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Cinematographer, Visual
+                    Designer, Creator</h1>
                 <p class="leading-relaxed mb-8">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing
                     tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami
                     snackwave tilde chillwave ugh. Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo
                     booth af fingerstache pitchfork.</p>
             </div>
+            <button
+                class="flex mx-auto mt-4 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Contact Us
+            </button>
         </div>
     </section>
 
     <footer class="text-gray-400 bg-gray-900 body-font">
         <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
             <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
-                <svg class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" fill="none" stroke="currentColor" stroke-linecap="round"
+                <svg class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" fill="none" stroke="currentColor"
+                     stroke-linecap="round"
                      stroke-linejoin="round" stroke-width="2"
                      viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                 </svg>
-                <span class="ml-3 text-xl">DeSimple</span>
+                <span class="ml-3 text-xl">Desimple Productions</span>
             </a>
             <p class="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">©
-                 2021
-                <a class="text-gray-500 ml-1" href="https://twitter.com/knyttneve" rel="noopener noreferrer"
+                2021
+                <a class="text-gray-500 ml-1" rel="noopener noreferrer"
                    target="_blank">@bitsandnibble</a>
+                <!--                <a class="text-gray-500 ml-1" href="https://twitter.com/knyttneve" rel="noopener noreferrer"-->
+                <!--                   target="_blank">@bitsandnibble</a>-->
             </p>
             <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
       <a class="text-gray-400">
